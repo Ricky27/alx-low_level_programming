4640912d3412e47002b3e4e145b8dc715d57d043
Ricky27 <ayenefam@gmail.com>
2021-10-27 01:53:57 -0700

    Low Level Programming Basics, Hello World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -  Main Block
+ * Return: return 0 everytime
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
